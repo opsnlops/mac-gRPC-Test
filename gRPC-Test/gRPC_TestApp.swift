2ff19a000fb3535dbbea080a71ea4b302bfcc769
April White <april@opsnlops.io>
2023-04-03 16:59:25 -0700

    Test

---
+++ gRPC-Test/gRPC_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gRPC_TestApp.swift
+//  gRPC-Test
+//
+//  Created by April White on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct gRPC_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
